d4eabad55c8b9e8523a11a8823056d12e35dbd7a
TheTurkeyDev <turkey2349@gmail.com>
2023-11-26 14:08:46 -0500

    2015 Day 4

---
+++ src/ts-code/2015/day-4-2015.ts
@@ -0,0 +1,15 @@
+import { input } from './day-4-2015-input';
+import * as crypto from 'crypto';
+
+const getHashNum = (start: string) => {
+    // eslint-disable-next-line functional/no-let
+    let num = 0, hash = '';
+    while (!hash.startsWith(start)) {
+        num++;
+        hash = crypto.createHash('md5').update(`${input}${num}`).digest('hex');
+    }
+    return num;
+};
+
+console.log(getHashNum('00000'));
+console.log(getHashNum('000000'));

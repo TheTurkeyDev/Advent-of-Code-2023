2bceb63992caf1a413e67fa68aa742b8f232e899
TheTurkeyDev <turkey2349@gmail.com>
2023-12-06 00:31:20 -0500

    2023 Day 6

---
+++ src/ts-code/2023/day-6.ts
@@ -0,0 +1,16 @@
+import { input } from './day-6-input';
+
+const lines = input.split('\n');
+const times = lines[0].substring(5).trim().split(' ').map(t => parseInt(t, 10));
+const distances = lines[1].substring(9).trim().split(' ').map(t => parseInt(t, 10));
+
+const part1 = times.map(t => Array.from({ length: t }).map((_, i) => i * (t - i)))
+    .reduce((prod, opts, index) => prod * Math.max(opts.filter(o => o > distances[index]).length, 1), 1);
+
+console.log(`Part 1: ${part1}`);
+
+const time = parseInt(lines[0].substring(5).replaceAll(' ', ''), 10);
+const distance = parseInt(lines[1].substring(9).replaceAll(' ', ''), 10);
+const options = Array.from({ length: time }).map((_, i) => i * (time - i)).filter(o => o > distance).length;
+
+console.log(`Part 2: ${options}`);

9d90397aa0e526da03f9a3b7a410e7bafd44498e
TheTurkeyDev <turkey2349@gmail.com>
2023-12-11 01:08:50 -0500

    Slightly more funtional

--- src/ts-code/2023/day-11.ts
+++ src/ts-code/2023/day-11.ts
@@ -6,59 +6,54 @@ const map = input.split('\n').map(l => l.split(''));
 const emptyRows = [] as number[];
 const emptyColumns = [] as number[];
 
-for (let y = 0; y < map.length; y++) {
-    let lineEmpty = true;
-    for (let x = 0; x < map[y].length; x++) {
-        if (map[y][x] !== '.') {
-            lineEmpty = false;
-            break;
-        }
-    }
+map.forEach((l, y) => {
+    const lineEmpty = l.reduce((notEmpty, c) => {
+        if (c !== '.')
+            return false;
+        return notEmpty;
+    }, true);
     if (lineEmpty)
         emptyRows.push(y);
-}
+});
 
-
-for (let x = 0; x < map[0].length; x++) {
-    let lineEmpty = true;
-    for (let y = 0; y < map.length; y++) {
-        if (map[y][x] !== '.') {
-            lineEmpty = false;
-            break;
-        }
-    }
+map[0].forEach((_, x) => {
+    const lineEmpty = map.reduce((notEmpty, _, y) => {
+        if (map[y][x] !== '.')
+            return false;
+        return notEmpty;
+    }, true);
     if (lineEmpty)
         emptyColumns.push(x);
-}
+});
 
 
 const getDistanceToAll = (startX: number, startY: number, expansionSize: number) => {
     let distance = 0;
-    for (let y = 0; y < map.length; y++) {
-        for (let x = 0; x < map[y].length; x++) {
-            if (map[y][x] === '.' || (startX === x && startY === y))
-                continue;
+    map.forEach((row, y) => {
+        row.forEach((char, x) => {
+            if (char === '.' || (startX === x && startY === y))
+                return;
             if (y < startY || (y === startY && x < startX))
-                continue;
+                return;
             const cols = emptyColumns.filter(c => c > Math.min(x, startX) && c < Math.max(x, startX)).length * (expansionSize - 1);
             const rows = emptyRows.filter(r => r > Math.min(y, startY) && r < Math.max(y, startY)).length * (expansionSize - 1);
             distance += Math.abs(y - startY) + Math.abs(x - startX) + cols + rows;
-        }
-    }
+        });
+    });
     return distance;
 };
 
 let part1 = 0;
 let part2 = 0;
 
-for (let y = 0; y < map.length; y++) {
-    for (let x = 0; x < map[y].length; x++) {
-        if (map[y][x] === '.')
-            continue;
+map.forEach((row, y) => {
+    row.forEach((char, x) => {
+        if (char === '.')
+            return;
         part1 += getDistanceToAll(x, y, 2);
         part2 += getDistanceToAll(x, y, 1000000);
-    }
-}
+    });
+});
 
 console.log(`Part 1: ${part1}`);
 console.log(`Part 2: ${part2}`);

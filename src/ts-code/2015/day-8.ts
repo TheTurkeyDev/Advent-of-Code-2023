9a8bb0d7f22c54a7d38d6d49b73b6396b990a92d
TheTurkeyDev <turkey@theturkey.dev>
2024-12-03 10:19:56 -0500

    2015 Day 8

---
+++ src/ts-code/2015/day-8.ts
@@ -0,0 +1,12 @@
+import { input } from './day-8-input';
+
+const orig = input.split('\n');
+const x = orig.map(l => l.substring(1, l.length - 1).replaceAll(/\\x[0-9a-f]{2}/g, '1').replaceAll(/\\"/g, '"').replaceAll(/\\\\/g, '\\'));
+const answer = x.reduce((prev, l, i) => prev + (orig[i].length - l.length), 0);
+
+console.log(`Part 1: ${answer}`);
+
+const encoded = orig.map(l => `"${l.replaceAll(/\\/g, '\\\\').replaceAll(/"/g, '\\"')}"`);
+const answer2 = encoded.reduce((prev, l, i) => prev + (l.length - orig[i].length), 0);
+console.log(`Part 2: ${answer2}`);
+//1485 Low

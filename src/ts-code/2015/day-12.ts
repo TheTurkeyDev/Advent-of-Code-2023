6ae28202226f7c47ec4407a928515d66a11b8418
TheTurkeyDev <turkey@theturkey.dev>
2024-12-04 18:48:44 -0500

    2015 Day 12

---
+++ src/ts-code/2015/day-12.ts
@@ -0,0 +1,23 @@
+import { input } from './day-12-input';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getValue = (elem: any, ignoreRed: boolean): number => {
+    return Object.keys(elem).reduce((sum, k) => {
+        const value = elem[k];
+        const typeOf = typeof value;
+        
+        if (typeOf === 'number')
+            return sum + (value as number);
+        if (typeOf === 'string')
+            return sum;
+
+        const keys = Object.keys(value);
+        const isArray = keys[0] === '0';
+        if (ignoreRed && !isArray && keys.some(k => value[k] === 'red'))
+            return sum;
+        return sum + getValue(value, ignoreRed);
+    }, 0);
+};
+
+console.log(`Part 1: ${getValue(input, false)}`);
+console.log(`Part 2: ${getValue(input, true)}`);
